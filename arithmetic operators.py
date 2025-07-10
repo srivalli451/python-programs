367cda27c29a968a0d13f171f3e72ff0ed687b3b
srivalli451 <dsrivalli45@gmail.com>
2025-07-10 10:22:01 +0530

    Add files via upload

---
+++ arithmetic operators.py
@@ -0,0 +1,18 @@
+""" 
+An Arithmetic operator is used to perform some mathematical/arithmetic Operations 
+the operators are +,-,,/,//,%,* 
+Note: input()is a function which takes input from user 
+""" 
+a = int(input("enter a number: ")) 
+b = int(input("enter a number: ")) 
+print("The addition ", a+b) 
+print("The subtraction ", a-b) 
+print("The mutliplication ", a*b) 
+print("The division ", a/b)#which returns the quotient 
+print("The floordivision ", a//b)#which returns the integral quotient part in a division
+print("The modulardivision ", a%b)#which returns the remainder part before the decimal 
+print("The power ",a**b)#which returns the power 
+""" 
+//#which returns the integral quotient part in a division 
+%#which returns the remainder part before the decimal 
+"""
